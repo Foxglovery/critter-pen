434afa63517db5e6571e46123e2894658a45ce59
Foxglovery <foxglovery@gmail.com>
2024-07-04 09:47:36 -0500

    planning the game

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Wordle from "./Components/Wordle/Wordle";
 
 function App() {
   const [solution, setSolution] = useState(null);
+  const [solvedSolutions, setSolvedSolutions] = useState();
   useEffect(() => {
     fetch("http://localhost:3000/solutions")
       .then((res) => res.json())
--- src/Components/Reminders.js
+++ src/Components/Reminders.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 
 export default function Reminders() {
+  // this will take the solutions once they are solved
+  // need to add parts of speech entry to solution db
+  // this will display them according to parts of speech
   const [reminders, setReminders] = useState([
     "theres something in the garden",
   ]);
